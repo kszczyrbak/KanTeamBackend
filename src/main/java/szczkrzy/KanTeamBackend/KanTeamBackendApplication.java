9630597c69ac08b0f473224506dae6517c362e58
kszczyrbak <kszczyrbak8@gmail.com>
2018-07-06 18:47:37 +0200

    second commit

---
+++ src/main/java/szczkrzy/KanTeamBackend/KanTeamBackendApplication.java
@@ -0,0 +1,12 @@
+package szczkrzy.KanTeamBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanTeamBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KanTeamBackendApplication.class, args);
+    }
+}
---
+++ src/test/java/szczkrzy/KanTeamBackend/KanTeamBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package szczkrzy.KanTeamBackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KanTeamBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
